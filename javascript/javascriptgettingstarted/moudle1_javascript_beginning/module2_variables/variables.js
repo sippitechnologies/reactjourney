f170f36f12478d4db0f39845ea911047b1e16996
sippitechnologies@gmail.com <sippitechnologies@gmail.com>
2021-05-14 20:34:50 +0530

    Implement variables names in javascript

---
+++ javascript/javascriptgettingstarted/moudle1_javascript_beginning/module2_variables/variables.js
@@ -0,0 +1,34 @@
+/*
+Variable represents with memory address in your app
+Naming Convention Rules for Javascript Variable
+It can be start with letter,_ and $
+It can't be start with number
+you can't use keyword as variable name
+you can't take space inside variable name
+
+*/
+
+//Example to use variable in javascript
+let name = "Deepak";
+let age =1;
+let price =10.5;
+let character = '1';
+let isAdult = true;
+
+character =10;
+//const keyword use
+//value can't be change the during program execuation
+const test=10;
+const value = "Deepak";
+showOutput(test);
+//var keyword uses
+
+showOutput(city);/*Can't give error in case of var keyword it doesn't matter where you use initialize 
+the variable it will work. var keyword scope is global that why it's dangerous to use
+Note : always try to use const let keyword for variables use in javascript
+*/
+var city= "Delhi"
+
+
+
+
